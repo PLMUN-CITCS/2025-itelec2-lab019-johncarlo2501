7a815d706a520ddb9d8d11a528f3415383330348
JOHN CARLO NUQUI <nuquijohncarlo_bsit@plmun.edu.ph>
2025-02-28 19:46:07 +0800

    Submitting Python Week 05 - Laboratory # 19

--- grade_calculator_functions.py
+++ grade_calculator_functions.py
@@ -1,10 +1,10 @@
 def get_student_score():
-    """Gets the student's score from user input."""
-    score = float(input("Enter the student's score: "))  
-    return score
+    
+    score = float(input("Enter your score: "))
+        if score <= 100:
+         return score 
 
-def calculate_grade(score):
-    """Determines the letter grade based on the score."""
+def calculate_grade(score): 
     if score >= 90:
         return 'A'
     elif score >= 80:
@@ -13,10 +13,10 @@ def calculate_grade(score):
         return 'C'
     elif score >= 60:
         return 'D'
-    else:
+    else: 
         return 'F'
 
-# Main program
-student_score = get_student_score()  
-student_grade = calculate_grade(student_score)  
-print(f"The student's grade is: {student_grade}")
+if __name__ == "__main__":
+    score = get_student_score()
+    grade = calculate_grade(score)
+    print(f"Your grade is: {grade}")
